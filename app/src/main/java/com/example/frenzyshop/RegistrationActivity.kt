9c6625c4530166e113bc709c29b724fa300c7fe6
Sasha <sanyashulgach@gmail.com>
2025-02-04 19:32:59 +0300

    добавление кнопок на регистрацию + перехват переменных

--- app/src/main/java/com/example/frenzyshop/RegistrationActivity.kt
+++ app/src/main/java/com/example/frenzyshop/RegistrationActivity.kt
@@ -1,6 +1,8 @@
 package com.example.frenzyshop
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,11 @@ class RegistrationActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val userName: EditText = findViewById(R.id.reg_name)
+        val userEmail: EditText = findViewById(R.id.reg_email)
+        val userPassword: EditText = findViewById(R.id.reg_password)
+        val userPasswordRepeat: EditText = findViewById(R.id.reg_password_repeat)
+        val button: Button = findViewById(R.id.reg_create_account_button)
     }
 }
