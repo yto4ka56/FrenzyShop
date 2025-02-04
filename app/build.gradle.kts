4478e0c3a6b87de5283a8d1a0392bdf06d68ad05
Sasha <sanyashulgach@gmail.com>
2025-02-04 18:24:02 +0300

    поменянные айдишники

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
