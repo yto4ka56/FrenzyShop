95997966d66fbd8f090f9ce331cdb549453c2ceb
maksimbelaev <maxim.beliaev18@gmail.com>
2025-02-04 18:23:00 +0300

    Добавлен переход на окно регистрации и кнопки

--- app/src/main/java/com/example/frenzyshop/RegistrationActivity.kt
+++ app/src/main/java/com/example/frenzyshop/RegistrationActivity.kt
@@ -1,24 +1,16 @@
 package com.example.frenzyshop
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AuthorizationActivity : AppCompatActivity() {
-
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_authorization)
-        val textView: TextView = findViewById(R.id.text_registration)
-        textView.setOnClickListener {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_registration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
